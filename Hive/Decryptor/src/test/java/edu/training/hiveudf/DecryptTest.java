ed92ff86265c8197201c53284823033deb2aeb5d
Sridhar <sreedharriyer@gmail.com>
2017-02-03 20:12:38 -0800

    Update DecryptTest.java

--- Hive/Decryptor/src/test/java/edu/training/hiveudf/DecryptTest.java
+++ Hive/Decryptor/src/test/java/edu/training/hiveudf/DecryptTest.java
@@ -5,8 +5,20 @@ import org.junit.Test;
 
 public class DecryptTest {
 
+	Decrypt udf;
+
+	@Before
+	public void setUp() {
+		udf = new Decrypt();
+	}
+
 	@Test
-	public void testDecrypt() {
-		assertEquals("Taylour Locke",new Decrypt().decrypt("70<QDGMJY6G{C[5"));
+	public void testID() {
+		assertEquals(new Text("3795"), udf.evaluate(new Text("1@+*`5")));
+	}
+
+	@Test
+	public void testName() {
+		assertEquals(new Text("Robert Larson"), udf.evaluate(new Text("4d.?+57Óí(56.-8")));
 	}
 }
