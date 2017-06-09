5ba2639634766f4617b217d3f552e6a3b55961a4
Sridhar <sreedharriyer@gmail.com>
2017-06-08 23:06:43 -0700

    formatted the source code for better readability

--- spark/src/main/scala/WordCount.scala
+++ spark/src/main/scala/WordCount.scala
@@ -1,14 +1,16 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+
 object WordCount {
-def main(args: Array[String]): Unit = {
-val inputPath = args(0)
-val outputPath = args(1)
-val sc = new SparkContext()
-val lines = sc.textFile(inputPath)
-val wordCounts = lines.flatMap {line => line.split(" ")}
-.map(word => (word, 1))
-.reduceByKey(_ + _)
-wordCounts.saveAsTextFile(outputPath)
-}
+  def main(args: Array[String]): Unit = {
+    val inputPath = args(0)
+    val outputPath = args(1)
+    val sc = new SparkContext()
+    val lines = sc.textFile(inputPath)
+
+    val wordCounts = lines.flatMap {line => line.split(" ")}
+                      .map(word => (word, 1))
+                      .reduceByKey(_ + _)
+                      wordCounts.saveAsTextFile(outputPath)
+  }
 }
