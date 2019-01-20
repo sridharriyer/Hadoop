b67078b40dc9ade9367a53ea56d9a07e45284329
Sridhar <sreedharriyer@gmail.com>
2019-01-20 22:21:20 +0530

    Update oozie_job.sh

--- oozie/oozie_job.sh
+++ oozie/oozie_job.sh
@@ -4,8 +4,6 @@ process_dir="/home/maria_dev/oozie/batchjobs/process"
 completed_dir="/home/maria_dev/oozie/batchjobs/completed"
 failed_dir="/home/maria_dev/oozie/batchjobs/failed"
 
-oozie_host="http://172.18.0.2:11000/oozie/"
-
 
 while true
 do
@@ -15,14 +13,14 @@ do
 			# write the logic here to check if the signal file is valid.
 			# if not valid move file to invalid signal directory and break loop
                         echo $signal
-			oozie_job_id=$(oozie job -oozie $oozie_host -DSignalName=$signal -config job.properties -run) 
+			oozie_job_id=$(oozie job -oozie http://172.18.0.2:11000/oozie/ -DSignalName=$signal -config job.properties -run) 
 			oozie_job_id=${oozie_job_id:5}
                         echo $oozie_job_id
 			sleep 10s
 			flag=true
 			while $flag
 			do
-				job_status=$(oozie job -poll $oozie_job_id -interval 1 -oozie $oozie_host -verbose)
+				job_status=$(oozie job -poll $oozie_job_id -interval 1 -oozie http://172.18.0.2:11000/oozie -verbose)
 				case $job_status in
 				RUNNING | SUBMITTED)
                                                 # This block is never reached; but as a precaution have put this
@@ -43,4 +41,3 @@ do
 			done
 		done
 done
-
