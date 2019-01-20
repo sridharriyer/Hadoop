08b44bb058ddb81a34d42a911db7b23764d5a71c
Sridhar <sreedharriyer@gmail.com>
2019-01-20 22:28:20 +0530

    Update oozie_job.sh

--- oozie/oozie_job.sh
+++ oozie/oozie_job.sh
@@ -3,41 +3,48 @@
 process_dir="/home/maria_dev/oozie/batchjobs/process"
 completed_dir="/home/maria_dev/oozie/batchjobs/completed"
 failed_dir="/home/maria_dev/oozie/batchjobs/failed"
+tmp_dir="/home/maria_dev/oozie/tmp"
 
+if [ -e $tmp_dir/oozie_workflow.lck ]
+then
+    echo "A process is in progress. Issue ps -ux | grep oozie.workflow.sh  to find the process id"
+    echo "If no process exists, delete the lck file to continue.."
+    exit
+else
+    touch $tmp_dir/oozie_workflow.lck
+fi
 
-while true
+while true:
 do
-        echo 'Waiting for signal...'
-        for signal_file in $(ls -tr $process_dir/ | head -1); do
+        for signal_file in $(ls -tr | head -1 $process_dir/*); do
 			signal=$(basename "$signal_file")
-			# write the logic here to check if the signal file is valid.
-			# if not valid move file to invalid signal directory and break loop
-                        echo $signal
 			oozie_job_id=$(oozie job -oozie http://172.18.0.2:11000/oozie/ -DSignalName=$signal -config job.properties -run) 
-			oozie_job_id=${oozie_job_id:5}
-                        echo $oozie_job_id
-			sleep 10s
+			oozie_job_id=$("$oozie_job_id" | awk '{print $2}')
+			if [$oozie_job_id -eq ''] then
+			   rm $tmp_dir/oozie_workflow.lck
+			   exit
+			else    
+			source /home/maria_dev/oozie/timer.sh $OOZIE_JOB_ID & echo $! > $tmp_dir/timer.pid &
+		    fi
 			flag=true
-			while $flag
+			while $flag:
 			do
-				job_status=$(oozie job -poll $oozie_job_id -interval 1 -oozie http://172.18.0.2:11000/oozie -verbose)
-				case $job_status in
+				status=$(oozie job -poll "$oozie_job_id" -oozie http://172.18.0.2:11000/oozie -verbose) 
+				case $status in
 				RUNNING | SUBMITTED)
-                                                # This block is never reached; but as a precaution have put this
-						sleep 5s
-                                                echo $job_status
-                                                flag=true
+						sleep 1m 
 						;;
 				SUCCEEDED) 
-						mv $process_dir/$signal_file $completed_dir/$signal_file
-                                                echo $job_status
+						mv $process_dir/$signal $completed_dir/$signal
 						flag=false
 						;;
 				KILLED | FAILED | SUSPENDED)
-						mv $process_dir/$signal_file $failed_dir/$signal_file
+						mv $process_dir/$signal $failed_dir/$signal
 						flag=false
 						;;
 				esac
+				kill $(<"$tmp_dir/timer.pid")
+				rm $tmp_dir/timer.pid
 			done
 		done
 done
